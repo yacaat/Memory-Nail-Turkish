83cbd3ae0588ef3b398e0de34b7d8dbed9382c65
yacaat <37926237+yacaat@users.noreply.github.com>
2019-04-10 11:30:38 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
+import sys
 import codecs
 import re
-f = codecs.open('türkçe.txt', encoding='utf-8')
-dic = f.read()
 
-s = r'\r\n[^sztdnmrlçşjkfvpb]*'
-number = '1123'
+try:
+    f = codecs.open('türkçe.txt', encoding='utf-8')
+    dic = f.read()
 
-for i in number:
-    if i == '1':
-        s = s + r'[td]'
-    elif i == '2':
-        s = s + r'n'
-    elif i == '3':
-        s = s + r'm'
-    s = s + r'[^sztdnmrlçşjkfvpb]*'
+    s = r'\r\n[^sztdnmrlçşjkfvpb]*'
+    number = sys.argv[1]
 
-s = s + r'\r\n'
+    for i in number:
+        if i == '1':
+            s = s + r'[td]'
+        elif i == '2':
+            s = s + r'n'
+        elif i == '3':
+            s = s + r'm'
+        elif i == '4':
+            s = s + r'r'
+        elif i == '5':
+            s = s + r'l'
+        elif i == '6':
+            s = s + r'[çjş]'
+        elif i == '7':
+            s = s + r'k'
+        elif i == '8':
+            s = s + r'[fv]'
+        elif i == '9':
+            s = s + r'bp'
+        s = s + r'[^sztdnmrlçşjkfvpb]*'
 
-phoneNumRegex = re.compile(s)
-mo = phoneNumRegex.findall(dic)
+    s = s + r'\r\n'
 
-for i in mo:
-    print(i.replace('\r\n',''))
+    phoneNumRegex = re.compile(s)
+    mo = phoneNumRegex.findall(dic)
 
-print("By Grok")
+    for i in mo:
+        print(i.replace('\r\n',''))
+
+    print("By Grok")
+
+except:
+    print("Parametre olarak direkt sayı girilmelidir.")
